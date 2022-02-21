377ad07275baace128c64aec466863b3bf119fa2
Ryan <ryan.maceachern@gmail.com>
2022-02-21 13:48:56 -0800

    set up additional routing for server

--- express_server.js
+++ express_server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
-const urlgDatabase = {
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
 app.get("/", (req, res) => {
   res.send("Hello");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
 });
 
 app.listen(PORT, () => {
