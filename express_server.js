40d0abc63248ee568ea32dfaf960f45f2b3f7c78
Ryan <ryan.maceachern@gmail.com>
2022-02-21 17:13:43 -0800

    IMPORTANT commit. Got urls/:shortURL link to operate correctly

--- express_server.js
+++ express_server.js
@@ -1,26 +1,37 @@
+const { application } = require('express');
 const express = require('express');
 const app = express();
 const PORT = 8080;
 
 app.set("view engine", "ejs");
 
-
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
 
 app.get("/", (req, res) => {
   res.send("Hello");
 });
 
 app.get("/urls", (req, res) => {
-  const urlDatabase = {
-    "b2xVn2": "http://www.lighthouselabs.ca",
-    "9sm5xK": "http://www.google.com"
+  const templateVars = {
+    "urls": urlDatabase
   };
-  res.render("urls_index", urlDatabase);
+  res.render("urls_index", templateVars);
 });
 
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = {
+    longURL: urlDatabase[req.params.shortURL],
+    shortURL: req.params.shortURL
+  };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
