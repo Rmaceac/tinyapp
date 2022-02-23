2fcd46988eea51a78284f7bc15d02139337d8a5e
Ryan <ryan.maceachern@gmail.com>
2022-02-22 16:44:41 -0800

    Added login form to header. server is setup to store login username as a cookie

--- express_server.js
+++ express_server.js
@@ -77,7 +77,12 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect("/urls");
 });
 
-
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie("username", username);
+  // console.log("USERNAME:", username);
+  res.redirect("/urls");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}.`);
