498852f1439a8485097ba24355983c4b214d8a0f
Ryan <ryan.maceachern@gmail.com>
2022-02-22 16:09:50 -0800

    basic linting

--- express_server.js
+++ express_server.js
@@ -64,11 +64,19 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect("/urls");
+});
+
 app.post("/urls/:shortURL/delete", (req, res) => {
   const shortURL = req.params.shortURL;
   delete urlDatabase[shortURL];
   res.redirect("/urls");
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}.`);
