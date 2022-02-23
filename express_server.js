84b87e2e54e8dd5cc979d424088653ee7588210c
Ryan <ryan.maceachern@gmail.com>
2022-02-22 16:24:05 -0800

    grouped 'edit link' form and submit button together on page

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@ const { application } = require('express');
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
 
 const app = express();
 app.use(morgan('dev'));
